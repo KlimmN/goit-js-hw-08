39df45ab0ff18318b8bc410c6a28cd110da39fea
Nikita <klimnikita20022001@gmail.com>
2025-11-03 12:10:36 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+
+const inputForm = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+
+inputForm.addEventListener('input', event => {
+  const value = event.currentTarget.value.trim();
+  output.textContent = value ? value : 'Anonymous';
+});
